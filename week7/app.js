5662cc894bbe0c70cace45aee084d422b34b4558
suhail <shaikmsuhail02@gmail.com>
2022-09-04 18:09:56 +0530

    added comments

--- week7/app.js
+++ week7/app.js
@@ -1,13 +1,19 @@
+// variable declarations
+
 let input = document.querySelector("input");
 let addBtn = document.querySelector(".add-btn");
 let taskDiv = document.querySelector(".tasks-div");
 let quote = document.querySelector(".quotes");
+
+// function to get user input
 
 let getTask = () => {
   let Tname = input.value;
   input.value = "";
   return Tname;
 };
+
+// function to add tasks
 
 let addTasks = (t) => {
   if (t == "") return;
@@ -35,6 +41,8 @@ let addTasks = (t) => {
 };
 
 let n = Math.floor(Math.random() * 500 + 1);
+
+// function to fetch quotes and author name from quotes API
 
 let getQuote = () => {
   axios
